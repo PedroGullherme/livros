42f4234be4177b84ca873dc93157c5806bb9a769
PedroGullherme <ppgrc@hotmail.com>
2023-07-05 20:16:56 -0300

    Back-end pagina de livros

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 
+const cors = require('cors');
+
 require('express-async-errors');
 
 const {routeLivros, routePaginas}= require('./routes')
 
-const variavelteste =process.env.TESTE
+const variavelteste = process.env.TESTE
 
 const routeCapitulos = require('./routes/routeCapitulos');
 
@@ -15,6 +17,14 @@ const errorMiddleware = require('./middlewares/errorMiddleware')
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  next();
+});
 
 app.use('/livros', routeLivros);
 
